d53f60fb3c8a8e3a327eba609c6763322c3d2f54
dersax <dersax@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-06 08:48:39 +0000

    * Bugs fixed: 	* Change theme now supports child theme 	* Fix css relative background 	* Category url in subdomain can use "category base" 	* Better Redirection with support paged, comment-page, etc
    * New features:
    	* Added Testing Mode.
    	* Detect wildcard subdomain has been set up or not
    	* Added "Find Subdomain" option for multicategories post
    	* %category% permalink can be removed completely or change to child category if exist
    	* User can shorten url with only single category
    	* Change Front page on subdomain with selected "page"
    	* Added how many count post show up on blog pages
    
    git-svn-id: https://plugins.svn.wordpress.org/main-category-as-subdomain/tags/2.0@1128572 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ main-category-as-subdomain.php
@@ -0,0 +1,83 @@
+<?php
+/*
+  Plugin Name: Main Category As Subdomain
+  Plugin URI: http://blogbintang.com
+  Description: Change your categories as subdomains. Please Set up * (wild card) Subdomain in your host.
+  Version: 2.0
+  Author: Bintang Taufik
+  Author URI: http://blogbintang.com
+  
+ * LICENSE
+  Copyright 2015 Bintang Taufik  (email : bintangtaufik@gmail.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+ 
+ 
+define( 'MCS_VERSION', 2.0 );
+define( 'MCS_DB_VERSION', 2.0 );
+include_once('class.subdomain.php');
+include_once('class.admin.subdomain.php');
+
+
+if(	is_blog_admin() ) {
+
+	function mcs_activation_plugin() {
+		$setting = array();
+		$settings[ 'bloginfo' ]						= 1;	
+		$settings[ 'home_url' ]						= 0;	
+		$settings[ 'redirect' ]						= 1;
+		$settings[ 'child_categories' ]				= 'main_categories_subdomains';
+		$settings[ 'recent_post' ]					= 0;
+		$settings[ 'using_index' ]					= 0;
+		$settings[ 'widget_recent_post' ]			= 0;
+		$settings[ 'widget_categories' ]			= 0;
+		$settings[ 'remove_category_permalink' ] 	= 0;
+		$settings[ 'multicat' ]						= 'by_id';	
+		$settings[ 'mode' ]							= 0; 		
+		$settings[ 'single_category_url' ]			= 0;					
+		$settings[ 'db_version' ] 					= MCS_DB_VERSION;
+		
+		add_option( 'mcs_subdomain_settings', $settings );
+		add_option( 'mcs_categories', array() );		
+	
+	}
+	
+	
+	register_activation_hook(__FILE__, 'mcs_activation_plugin' );	
+	
+	//upgrade
+	mcs_admin::upgrade();
+	
+	//Initialize ADMIN AREA
+	new mcs_admin();
+	
+} else {
+
+	$mcs_execute = new mcs_subdomain();
+	$mcs_execute->ACTION();
+	$mcs_execute->FILTER();
+
+	function mcs_home_url() {
+		global $mcs_execute;
+		if ( $mcs_execute->subdomain )
+			return $mcs->bloginfo_url();
+		else
+			return home_url();
+	}
+
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,76 @@
+=== Main Category As Subdomain ===
+Contributors: dersax
+Donate link: 
+Tags: subdomain, subdomains , category ,categories
+Requires at least: 3.9.1
+Tested up to: 4.1.1
+Stable tag: 2.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This Plugin allows users to setup there main categories as subdomains
+
+== Description ==
+
+This Plugin allows users to setup Main categories as subdomains, 
+
+Features
+
+*   Setup main categories as subdomains
+*   Custom themes for each subdomains
+*   Redirect Old Url
+*   Change Child Categories to Categories Or Subdomain
+*	Change Front page on subdomain
+*   Many More..
+
+== Installation ==
+
+= A. Configuring Wildcard Subdomains =
+
+*   Make a sub-domain named "*" (wildcard) at your CPanel (*.example.com). Make sure to point this at the same folder location where your wordpress folder is located.
+
+= B. Upload This Plugin into wordpress =
+*   Upload this plugin to the `/wp-content/plugins/` directory
+*   Or Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+1. Don't have many questions, but if you have, please send to bintangtaufik@gmail.com
+
+== Screenshots ==
+
+1. Main Page Where you can change selected category to subdomain or change the theme
+2. All Settings on subdomain
+
+== Changelog ==
+
+= 2.0 =
+
+* Bugs fixed:
+	* Change theme now supports child theme
+	* Fix css relative background
+	* Category url in subdomain can use "category base"
+	* Better Redirection with support paged, comment-page, etc
+	
+
+* New features:
+	* Added Testing Mode.
+	* Detect wildcard subdomain has been set up or not
+	* Added "Find Subdomain" option for multicategories post
+	* %category% permalink can be removed completely or change to child category if exist
+	* User can shorten url with only single category
+	* Change Front page on subdomain with selected "page"
+	* Added how many count post show up on blog pages
+	
+
+= 1.0 =
+* Born
+
+
+== Upgrade Notice ==
+
+= 2.0 =
+*Upgrade db option mcs_categories
+
+= 1.0 =
+* None
